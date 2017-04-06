c00d1cb2419cb91833a29fa205b0560e310a391d
Ross Warren <peregrine.warren@physics.ox.ac.uk>
2017-04-06 17:15:44 +0100

    new tsp

--- K2636.py
+++ K2636.py
@@ -10,6 +10,7 @@
 import visa
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 
 ########################################################################
 class K2636():
@@ -66,7 +67,7 @@ class K2636():
 			print('[%s]\t-->' %line_count, line, end='')
 			line_count += 1
 		self.write('endscript')
-		print ('----------SENT TO K2636-----------\n')
+		print ('\n----------SENT TO K2636-----------\n')
 				
 	#----------------------------------------------------------------------		
 	def runTSP(self):
@@ -78,11 +79,13 @@ class K2636():
 	def readBuffer(self):
 		'''Read specified buffer in keithley memory and return a pandas array'''
 		vg = [float(x) for x in self.query('printbuffer(1, smub.nvbuffer1.n, smub.nvbuffer1.sourcevalues)').split(',')]
+		ig = [float(x) for x in self.query('printbuffer(1, smub.nvbuffer1.n, smub.nvbuffer1.readings)').split(',')]
 		vd = [float(x) for x in self.query('printbuffer(1, smua.nvbuffer1.n, smua.nvbuffer1.sourcevalues)').split(',')]
 		c = [float(x) for x in self.query('printbuffer(1, smua.nvbuffer1.n, smua.nvbuffer1.readings)').split(',')]
 		
-		df = pd.DataFrame({'Gate Voltage [V]': vg, 'Channel Voltage [V]' : vd, 'Channel Current [A]': c})
+		df = pd.DataFrame({'Gate Voltage [V]': vg, 'Channel Voltage [V]' : vd, 'Channel Current [A]': c, 'Gate Leakage [A]': ig})
 		return df
+
 		
 		
 ########################################################################
@@ -93,15 +96,58 @@ def uploadTSP():
 	
 	rm = visa.ResourceManager('@py') #use py-visa backend
 	keithley = K2636(rm, address='ASRL/dev/ttyUSB0', read_term='\n', baudrate=57600)
+	
+	#-------------------------------------------------------------
+	sample = 'znpc-5wtpc-20um-3'
+	
+	#-------------------------------------------------------------
+	#print ('Uploading TSP script: ')
+	#keithley.loadTSP('TSP-scripts/iv-sweep.tsp')
+	#keithley.runTSP()
+	#df = keithley.readBuffer()
+	#output_name = str(sample + '-iv-sweep.csv')
+	#df.to_csv(output_name, sep='\t', index=False)	
+	
 	print ('Uploading TSP script: ')
-	keithley.loadTSP('TSP-scripts/transistor-tools2.tsp')
+	keithley.loadTSP('TSP-scripts/output-charact.tsp')
 	keithley.runTSP()
 	df = keithley.readBuffer()
-	output_name = 'znpc-reusedsub-20um-2.csv'
-	df.to_csv(output_name, sep='\t', index=False)	
+	output_name = str(sample + '-output.csv')
+	df.to_csv(output_name, sep='\t', index=False)
+	
+	print ('Uploading TSP script: ')
+	keithley.loadTSP('TSP-scripts/transfer-charact.tsp')
+	keithley.runTSP()
+	df = keithley.readBuffer()
+	output_name = str(sample + '-transfer.csv')
+	df.to_csv(output_name, sep='\t', index=False)
+	
+	print ('Uploading TSP script: ')
+	keithley.loadTSP('TSP-scripts/gate-leakage.tsp')
+	keithley.runTSP()
+	df = keithley.readBuffer()
+	output_name = str(sample + '-gate-leakage.csv')
+	df.to_csv(output_name, sep='\t', index=False)		
+	
+	#------------------------------------------------------------
+	
 	rm.close()
 	
 	#------------------------------------------------------------
+	
+	df1 = pd.read_csv(str(sample+'-iv-sweep.csv'),'\t')
+	plt.plot(df1['Channel Voltage [V]'], df1['Channel Current [A]'], '.')
+	plt.show()
+	df2 = pd.read_csv(str(sample+'-output.csv'),'\t')
+	plt.plot(df2['Channel Voltage [V]'], df2['Channel Current [A]'], '.')
+	plt.show()
+	df3 = pd.read_csv(str(sample+'-transfer.csv'),'\t')
+	plt.plot(df3['Gate Voltage [V]'], df3['Channel Current [A]'], '.')
+	plt.show()
+	df4 = pd.read_csv(str(sample+'-gate-leakage.csv'),'\t')
+	plt.plot(df4['Gate Voltage [V]'], df4['Gate Leakage [A]'], '.')
+	plt.show()
+	
 	print ('END')
 
 if __name__ == '__main__':
